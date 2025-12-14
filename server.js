af271dd9641c6d454db03cef5bf4e627cc566713
salonmoney2025 <246838815+salonmoney2025@users.noreply.github.com>
2025-12-14 10:18:50 +0000

    modification on the backend .env

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
-  'https://finalmoney.vercel.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
